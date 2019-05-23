0b5cb2868171f9e4bb24aeff5fa28c6de14fdf46
Neeraj-Chhabra <neeraj.chhabra@dal.ca>
2019-05-23 11:59:03 -0300

    task2

--- task2.py
+++ task2.py
@@ -1,4 +1,15 @@
 from task1 import L33t
 import os
 
-os.system("ls -l")
+# get the dictionary for the files 
+myval=os.system("ls -l| awk \'{print $9}\'  ")
+mykey=os.system("pwd")
+mydic={}
+mydic[mykey]=myval
+print(mydic)
+
+
+#get the length of the string
+for var in myval:
+	mylst[var]= len(var)
+
