f2fcb6f6ad5ec7411e56b848a42ffb6b232c4558
Neeraj-Chhabra <neeraj.chhabra@dal.ca>
2019-05-23 11:44:10 -0300

    changes in task1 for import and started working on task2

--- task1.py
+++ task1.py
@@ -1,29 +1,30 @@
 #assuming the text to be copied is in a file
 import string
 import os
-try:
-	init_page=int(input("Enter the initial page number to  be copied = "))
-	end_page=int(input("Enter the final page to be copied = "))
-	target_file_name=input("Enter the file name where text to be copied = ")
-	target_location=input("Enter the target location(optional) = ")
+if __name__ == "__main__":
+	try:
+		init_page=int(input("Enter the initial page number to  be copied = "))
+		end_page=int(input("Enter the final page to be copied = "))
+		target_file_name=input("Enter the file name where text to be copied = ")
+		target_location=input("Enter the target location(optional) = ")
 	
-	if target_location=='':
-		tl=target_file_name
-		print("Target location is ", tl)
-	else:
-		tl=target_location/target_file_name
-		print("Target location is ", tl)
+		if target_location=='':
+			tl=target_file_name
+			print("Target location is ", tl)
+		else:
+			tl=target_location/target_file_name
+			print("Target location is ", tl)
 
-except:
-	pass
-tl='text.txt'
-def copy_pages(ip,ep,tl):
-	end_line=25*ep
-	start_line=25*ip+1
-	os.system("sed -n '%s,75p' Book1.txt >> text.txt" %ip, )
-	print("the new file created is text.txt")
+	except:
+		pass
+	tl='text.txt'
+	def copy_pages(ip,ep,tl):
+		end_line=25*ep
+		start_line=25*ip+1
+		os.system("sed -n '%s,75p' Book1.txt >> text.txt" %ip )
+		print("the new file created is text.txt")
 		
-copy_pages(init_page,end_page,tl)
+	copy_pages(init_page,end_page,tl)
 
 def L33t(word):
 	for char in word:
@@ -45,9 +46,3 @@ for line in myfile:
 	for word in line:
 		word=L33t(word)
 myfile.close()
-
-
-
-
-
-
---
+++ task2.py
@@ -0,0 +1,4 @@
+from task1 import L33t
+import os
+
+os.system("ls -l")
